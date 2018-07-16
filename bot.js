ea7eb4a8d9bf40a49b149c9a3fe4d21e75955eb4
hijab232 <37248973+hijab232@users.noreply.github.com>
2018-07-16 03:14:01 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,109 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+
+  client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 100000 ;
+    var setGame = [`Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
+
+});
+
+client.on('message', function(message) {
+  if (message.channel.type === "dm") {
+      if (message.author.id === client.user.id) return;
+      var norElden = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setTimestamp()
+          .setTitle('``رساله في خاص البوت``')
+          .setThumbnail(`${message.author.avatarURL}`)
+          .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+          .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+      client.channels.get("458493668395843585").send({ embed: norElden });
+  }
+});
+
+    client.on("guildCreate", guild => {
+    client.channels.get("458493668395843585").send(' ***  البوت  ***   **دخل في**   ***[ ' + `${guild.name}` + ' ]***   ,   **  الأونر  **  ' + ' ***[ ' + '<@' + `${guild.owner.user.id}` + '>' + ' ]***  **|**  ***[ ' + '<' + `${guild.owner.user.username}` + '>' + ' ]***')
+    });
+    
+    client.on("guildDelete", guild => {
+    client.channels.get("458493668395843585").send(' ***  البوت  ***   **دخل في**   ***[ ' + `${guild.name}` + ' ]***   ,   **  الأونر  **  ' + ' ***[ ' + '<@' + `${guild.owner.user.id}` + '>' + ' ]***  **|**  ***[ ' + '<' + `${guild.owner.user.username}` + '>' + ' ]***')
+    });
+
+
+client.on('message', msg => {
+    if (msg.content === 'السلام عليكم') {
+      msg.channel.sendMessage(' **وعليكم السلام ورحمة الله وبركاته**');
+    }
+  });
+
+  client.on('message', msg => {
+    if (msg.content === '>>>>>>>>') {
+      msg.reply('هلا!');
+    }
+
+  });
+  
+  client.on('message', message => {
+  var prefix = "!";
+  if(message.content.startsWith(prefix + "invbot")) { 
+  message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080374975`);
+  }
+  });
+
+client.on('message', message => {
+    var prefix = "!";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**!bc <لبرساله>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+    
+    client.login(process.env.BOT_TOKEN);
