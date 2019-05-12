e2f8cf50f06f65c3fcbc52807f5bbd49840c475f
hijab232 <37248973+hijab232@users.noreply.github.com>
2019-05-13 02:43:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,15 +72,30 @@ client.on('message', function(message) {
   }
   });
 
-client.on('message', async message => {
-  let args = message.content.slice(3);
-  if(message.content.startsWith(prefix + 'bc')) {
-    if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return message.channel.send('Required Administrator Permission')
-       message.guild.members.forEach(m => {
-      
-      m.send(args.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', message.author.username))
-    })
-  }
-})
+client.on('message' , message => {
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(prefix + "bc")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | `$rolebc role message`")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(`${codes}`)
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
 
 client.login(process.env.BOT_TOKEN);
